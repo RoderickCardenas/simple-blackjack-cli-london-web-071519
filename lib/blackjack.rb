cfc211fbdbc3184ba1ee27ea683442fbf26c1a71
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-16 12:04:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,7 +7,8 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  puts "Your cards add up to  #{card_total}"
+  total = card_total
+  puts "Your cards add up to  #{total}"
 end
 
 def prompt_user
